1b1a8847133235d0b8ca8e7d369b92d670fa05be
tokyo <contact@spech.dev>
2025-07-06 08:46:12 +0200

    change block public policy to false so that cloudfront can access it

--- s3.tf
+++ s3.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_ownership_controls" "web_hosting" {
 resource "aws_s3_bucket_public_access_block" "web_hosting" {
   bucket                  = aws_s3_bucket.web_hosting.id
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
