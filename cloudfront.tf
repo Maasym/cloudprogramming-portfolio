93cc5fbd2cf6ed5f855d3f72acc9bc1e3daf5295
tokyo <contact@spech.dev>
2025-07-06 08:28:14 +0200

    add cloudfront and s3 policy for cloudfront access

---
+++ cloudfront.tf
@@ -0,0 +1,65 @@
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "oac-for-greenleaf-web"
+  description                       = "Origin Access Control for the Greenleaf website S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled = true 
+
+  origin {
+    domain_name              = aws_s3_bucket.web_hosting.bucket_regional_domain_name
+    origin_id                = "S3-${aws_s3_bucket.web_hosting.id}"
+    
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id = "S3-${aws_s3_bucket.web_hosting.id}"
+
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = var.tags
+}
