848340becf777d33fbcd86c73e4a3e4d7e69f844
tokyo <contact@spech.dev>
2025-07-06 10:44:05 +0200

    add waf

---
+++ waf.tf
@@ -0,0 +1,93 @@
+resource "aws_wafv2_web_acl" "default" {
+  name        = "greenleaf-website-acl-${var.environment}"
+  description = "WAF for GreenLeaf static website"
+  scope       = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+        rule_action_override {
+          name = "SizeRestrictions_BODY"
+          action_to_use {
+            count {}
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "Rate-Limit-Rule"
+    priority = 2
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 500
+        aggregate_key_type = "IP"
+
+        forwarded_ip_config {
+          fallback_behavior = "MATCH"
+          header_name       = "X-Forwarded-For"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "RateLimitRule"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "GeoBlockHighRiskRegions"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      geo_match_statement {
+        country_codes = ["RU", "CN", "KP", "IR"]
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "GeoBlockRule"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "GreenLeafWAF"
+    sampled_requests_enabled   = true
+  }
+
+  tags = var.tags
+}
