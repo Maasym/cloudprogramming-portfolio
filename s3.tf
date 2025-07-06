c521ef32e83b81f5ac8219b35770dae397c10f57
tokyo <contact@spech.dev>
2025-07-06 11:23:06 +0200

    remove bucket policy

--- s3.tf
+++ s3.tf
@@ -18,25 +18,3 @@ resource "aws_s3_bucket_public_access_block" "web_hosting" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "web_hosting_policy" {
-  bucket = aws_s3_bucket.web_hosting.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontServicePrincipalReadOnly",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        },
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.web_hosting.arn}/*",
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
-          }
-        }
-      }
-    ]
-  })
-}
