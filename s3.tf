0ed3d41460723711a496929ca3edeab9a9b2913d
tokyo <contact@spech.dev>
2025-07-06 08:53:42 +0200

    change s3 restrict public buckets to false for cloudfront to have access

--- s3.tf
+++ s3.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_public_access_block" "web_hosting" {
   block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "web_hosting_policy" {
